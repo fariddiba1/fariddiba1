82e34856ba79d838b54860cbbaf47928b8982cb2
fariddiba1 <150931076+fariddiba1@users.noreply.github.com>
2024-02-22 17:40:14 +0330

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,31 @@
+/* This file is part of MCF Gthread.
+ * See LICENSE.TXT for licensing information.
+ * Copyleft 2022, LH_Mouse. All wrongs reserved.  */
+
+#include "../mcfgthread/cxa.h"
+#include "../mcfgthread/exit.h"
+#include <assert.h>
+#include <stdio.h>
+
+static
+void
+atexit_second(void* ptr)
+  {
+    __MCF__Exit(*(int*) ptr);
+  }
+
+static
+void
+atexit_first(void* ptr)
+  {
+    *(int*) ptr = 0;
+  }
+
+int
+main(void)
+  {
+    static int value = 42;
+    __MCF_cxa_at_quick_exit(atexit_second, &value, __MCF_nullptr);
+    __MCF_cxa_at_quick_exit(atexit_first, &value, __MCF_nullptr);
+    __MCF_quick_exit(1);
+  }
